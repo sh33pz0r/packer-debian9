d1ee31360cf038b1e76427683c7ae5e438dd7a3c
sh33pz0r <sh33pz0r@mail.iou>
2018-07-26 21:05:50 +0300

    added ubuntu18

--- build.sh
+++ build.sh
@@ -40,10 +40,14 @@ if [[ $1 == "ec2ami" ]] ; then
     aws --region $AWS_REGION ec2 describe-images --image-ids $SOURCE_AMI > /dev/null || { echo "ERROR: SOURCE_AMI is not available"; exit 1; }
     shift 1
     packer build $@ ./packer-debian9-ec2ami.json
-elif [[ $1 == "qemu-kvm" ]] ; then
+elif [[ $1 == "debian" ]] ; then
     shift 1
     export PACKER_LOG=1
     packer build $@ ./packer-debian9-qemu-kvm.json
+elif [[ $1 == "ubuntu" ]] ; then
+    shift 1
+    export PACKER_LOG=1
+    packer build $@ ./packer-ubuntu18.04.1-qemu-kvm.json
 else
     usage
 fi
